ae98da0814d555abeaf0c232f14a42d5f58cef12
myUserName <xita3423@colorado.edu>
2019-02-01 11:01:04 -0700

    Creating practice.sh file & storing regex_practice.sh

---
+++ lab2_scripts.sh
@@ -0,0 +1,23 @@
+0#!/bin/bash
+# Authors : Xingxuan Tang
+# Date: 1/1/2019
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+
+#!/bin/bash
+# Author : Your Name
+# Date: 1/1/2019
+# Script follows here:
+
+
+echo "File name: "
+read filename
+echo "Regular expression"
+read exp
+
+grep -E -c "[0-9]{3}-" regex_practice.txt 
+grep -E -c ".com$" regex_practice.txt
+grep -E "^[300]{3}-" regex_practice.txt >  phone_results.txt
+grep -E  "^.+@geocities\.com$" regex_practice.txt > email_results.txt
+grep -E $exp regex_practice.txt > command_results.txt
